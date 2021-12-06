08f5c5082a16a89f5d6473ba613684aeda33367c
ANameBehindTheNickname <name.behind.nickname@gmail.com>
2021-12-06 12:48:30 +0200

    Compose types in SceneDelegate to see the progress visually

--- DarkRoastedBeans/SceneDelegate.swift
+++ DarkRoastedBeans/SceneDelegate.swift
@@ -10,7 +10,21 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let navigation = UINavigationController()
+        let brewingMachineVM = BrewingMachineViewModel(
+            styles: ["Coffee 1", "Coffee 2", "Coffee 3", "Coffee 4", "Coffee 5"],
+            sizes: ["Small", "Medium", "Large"],
+            extras: ["Sugar", "Milk"]
+        )
+        
+        let flow = DrinkBrewingFlow(navigation: navigation, brewingMachineVM: brewingMachineVM)
+        flow.start()
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = navigation
+        window?.makeKeyAndVisible()
     }
 }
 
